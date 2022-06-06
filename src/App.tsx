bfb32f77e3eda4bfe799ae3e0276c17ba3282368
Virginia Cepeda <virginiamcepeda@gmail.com>
2022-06-05 22:29:43 -0300

    Configure Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import DocumentPage from "./components/Document/DocumentPage";
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: "https://graphql.sketch.cloud/api",
+    cache: new InMemoryCache(),
+  });
+};
+
+const client = createApolloClient();
+
 const App: React.FC = () => (
   <Router>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<DocumentPage />} />
       </Routes>
+    </ApolloProvider>
   </Router>
 );
 
